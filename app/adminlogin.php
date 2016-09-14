07645355a5023e110f7a85909dfae9dc5daf9e83
Narendra-Dodwaria <dodwaria.nd536@gmail.com>
2016-09-15 01:26:24 +0530

    Admin panel added

---
+++ app/adminlogin.php
@@ -0,0 +1,15 @@
+<?php
+	$username = $_POST["uname"];
+	$password = $_POST["psw"];
+	//echo $username." : ".$password;
+	if($username=="admin123")
+	{
+		if($password=="adminlogin")
+		{
+			echo "login succesful";
+			header("Location: /app/adminDashboard.html");
+		}
+		else
+			echo "You are not admin !";
+	}
+?>
